455a6835e2644a3b5c6b2ef187c61cf3137d8d86
Wanderson Silva <wandersonsilva768@gmail.com>
2020-12-10 06:14:10 -0300

    feat: repassando datas para o componente App

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
 
     <div class="date_inputs">
       <DateInput
+        v-model="dateA"
         for_id="datainicial"
         label="Data Inicial" />
 
       <DateInput
+        :value="dateB"
+        v-model="dateB"
         for_id="datafinal"
         label="Data Final" />
     </div>
 
     <div class="date_result">
-      <input type="text" placeholder="anos; meses; semanas; dias" readonly />
+      <input type="text" placeholder="anos; meses; semanas; dias" :value="isNaN(dateTimeA)" readonly />
     </div>
   </main>
 </template>
@@ -24,8 +27,49 @@
   export default {
     name: 'App',
 
+    data () {
+      return {
+        dateA: '',
+        dateB: ''
+      };
+    },
+
+    methods: {
+      ISO8601Date (date) {
+        return date.split('/').reverse().join('/');
+      },
+
+      getTimeFromDate (date) {
+        return new Date(date).valueOf();
+      }
+    },
+
+    computed: {
+      dateTimeA () {
+        const { dateA, getTimeFromDate, ISO8601Date } = this;
+
+        return getTimeFromDate(ISO8601Date(dateA));
+      },
+
+      dateTimeB () {
+        const { dateB, getTimeFromDate, ISO8601Date } = this;
+
+        return getTimeFromDate(ISO8601Date(dateB));
+      }
+    },
+
     components: {
       DateInput
+    },
+
+    watch: {
+      dateA (current) {
+        console.log('dataA', current);
+      },
+
+      dateB (current) {
+        console.log('dataB', current);
+      }
     }
   }
 </script>
@@ -38,21 +82,33 @@
     height: auto;
     max-width: 400px;
     padding: 12px 5px;
-    background-color: #fff;
-    box-shadow: 0 1px 3px #aaa;
+    background-color: #14273d;
+    box-shadow: 0 10px 50px -10px #222;
 
     @extend %flex-center;
     flex-direction: column;
     @include border-radius(3px);
 
     h1.title {
-      color: #444;
+      color: #4475e2;
       padding: 13px 0;
       font-size: 14pt;
       text-align: center;
       margin-bottom: 10px;
       text-transform: uppercase;
       font-family: 'Nunito', sans-serif;
+    }
+
+    .date_inputs {
+      width: 100%;
+      padding: 5px;
+
+      @extend %flex-spacearound-center;
+
+      @media (max-width: 320px) {
+        height: 120px;
+        flex-direction: column;
+      }
     }
 
     .date_result {
@@ -70,6 +126,7 @@
         padding: 4px 0;
         font-size: 10pt;
         text-align: center;
+        background: transparent;
         border: solid 1px #ccc;
         text-transform: uppercase;
         font-family: 'Quicksand', sans-serif;
--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -4,105 +4,142 @@
       required
       type="text"
       :id="for_id"
+      maxlength="10"
       inputmode="tel"
-      maxlength="10" />
-    <label :for="for_id"> {{ label }} </label>
-    <div class="active_border"></div>
+      @input="changeDate" />
+
+    <label :for="for_id">
+      {{ label }}
+    </label>
+
+    <div class="active_border" :class="{ error: !dataFilledProperly }"></div>
   </div>
 </template>
 
 <script>
-export default {
- name: 'DateInput',
+  export default {
+    name: 'DateInput',
 
- props: {
-   for_id: {
-     type: String,
-     required: true
-   },
+    data () {
+      return {
+        currentDate: ''
+      };
+    },
 
-   label: {
-     type: String,
-     required: true
-   }
- }
-}
+    methods: {
+      changeDate ($event) {
+        this.currentDate = $event.target.value;
+      }
+    },
+
+    computed: {
+      dataFilledProperly () {
+        const { currentDate } = this;
+
+        return currentDate.length && /(\d{2})\/(\d{2})\/(\d{4})/.test(currentDate);
+      }
+    },
+
+    props: {
+      for_id: {
+        type: String,
+        required: true
+      },
+
+      label: {
+        type: String,
+        required: true
+      }
+    },
+
+    watch: {
+      dataFilledProperly (current) {
+        if (current) {
+          this.$emit('input', this.currentDate);
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
   @import '../utils/scss/global.scss';
 
-  .date_inputs {
-    width: 100%;
-    padding: 5px;
+  .date_input {
+    width: 50%;
+    height: 25px;
+    position: relative;
+    @extend %flex-center;
 
-    @extend %flex-spacearound-center;
+    @media (max-width: 320px) {
+      width: 90%;
+      height: 35px;
+    }
 
-    .date_input {
-      width: 50%;
-      height: 25px;
-      position: relative;
-      @extend %flex-center;
+    .active_border {
+      width: 80%;
+      height: 2px;
+      bottom: -2px;
+      position: absolute;
 
-      .active_border {
-        width: 80%;
+      &.error:after,
+      &.error:before {
+        background: red;        
+      }
+
+      &:after,
+      &:before {
+        content: '';
+        top: 0px;
+        width: 0%;
         height: 2px;
-        bottom: -2px;
         position: absolute;
+        transition: .2s ease;
+        background-color: #fff;
+      }
 
-        &:after,
-        &:before {
-          content: '';
-          top: 0px;
-          width: 0%;
-          height: 2px;
-          position: absolute;
-          transition: .2s ease;
-          background-color: #00A6FB;
+      &:after  { left: 50%; }
+      &:before { right: 50%; }
+    }
+
+    label,
+    input {
+      color: #fff;
+      width: 90%;
+      font-size: 12pt;
+      text-align: center;
+      background: transparent;
+      touch-action: manipulation;
+    }
+
+    label {
+      cursor: pointer;
+      padding: 6px 4px;
+      overflow: hidden;
+      position: absolute;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      text-transform: uppercase;
+      transition: .2s ease-in-out;
+      font-family: 'Quicksand', sans-serif;
+    }
+
+    input {
+      border: none;
+      padding: 4px 4px;
+      font-family: 'Nunito', sans-serif;
+
+      &:focus,
+      &:valid {
+        & ~ label {
+          opacity: .6;
+          transform-origin: center bottom;
+          transform: translate(0, -25px) scale(.6);
         }
 
-        &:after  { left: 50%; }
-        &:before { right: 50%; }
-      }
-
-      label,
-      input {
-        width: 90%;
-        font-size: 12pt;
-        text-align: center;
-        touch-action: manipulation;
-      }
-
-      label {
-        cursor: pointer;
-        padding: 6px 4px;
-        overflow: hidden;
-        position: absolute;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        text-transform: uppercase;
-        transition: .2s ease-in-out;
-        font-family: 'Quicksand', sans-serif;
-      }
-
-      input {
-        border: none;
-        padding: 4px 4px;
-        font-family: 'Nunito', sans-serif;
-
-        &:focus,
-        &:valid {
-          & ~ label {
-            color: #00A6FB;
-            font-size: 10pt;
-            transform-origin: center bottom;
-            transform: translate(0, -20px) scale(.9);
-          }
-
-          & ~ .active_border:after,
-          & ~ .active_border:before {
-            width: 50%;
-          }
+        & ~ .active_border:after,
+        & ~ .active_border:before {
+          width: 50%;
         }
       }
     }
